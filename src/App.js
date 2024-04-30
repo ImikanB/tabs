50e32b7351b7ff3a8f79f055f126e2aa5f8732f2
Imikan10 <basseyimikan10@gmail.com>
2024-04-30 10:01:09 +0100

    tabs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,132 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+
+import { names } from './data'
 
 function App() {
+  const [data, setData] = useState('bassey')
+
+  const handleClick = (category) => {
+    setData(category)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <section className='container'>
+        <div className='bar'>
+          <button
+            className={`nav ${data === 'bassey' ? 'active' : ''}`}
+            onClick={() => handleClick('bassey')}
+          >
+            bassey
+          </button>
+          <button
+            className={`nav ${data === 'olisa' ? 'active' : ''}`}
+            onClick={() => handleClick('olisa')}
+          >
+            olisa
+          </button>
+          <button
+            className={`nav ${data === 'miracle' ? 'active' : ''}`}
+            onClick={() => handleClick('miracle')}
+          >
+            miracle
+          </button>
+        </div>
+
+        {names.map((person) => {
+          const {
+            job,
+            name,
+            date,
+            para1,
+            para2,
+            para3,
+            para4,
+            category,
+            index,
+          } = person
+          if (data === category || data === '') {
+            return (
+              <article key={index}>
+                <h3 className='head'>{job}</h3>
+                <span>{name}</span>
+                <p className='date'>{date}</p>
+
+                <div>
+                  <div className='desc'>
+                    <svg
+                      stroke='currentColor'
+                      fill='currentColor'
+                      stroke-width='0'
+                      viewBox='0 0 448 512'
+                      className='icon'
+                      height='1em'
+                      width='1em'
+                      xmlns='http://www.w3.org/2000/svg'
+                    >
+                      <path d='M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34zm192-34l-136-136c-9.4-9.4-24.6-9.4-33.9 0l-22.6 22.6c-9.4 9.4-9.4 24.6 0 33.9l96.4 96.4-96.4 96.4c-9.4 9.4-9.4 24.6 0 33.9l22.6 22.6c9.4 9.4 24.6 9.4 33.9 0l136-136c9.4-9.2 9.4-24.4 0-33.8z'></path>
+                    </svg>
+                    <p className='detail'>{para1}</p>
+                  </div>
+
+                  <div className='desc'>
+                    <svg
+                      stroke='currentColor'
+                      fill='currentColor'
+                      stroke-width='0'
+                      viewBox='0 0 448 512'
+                      className='icon'
+                      height='1em'
+                      width='1em'
+                      xmlns='http://www.w3.org/2000/svg'
+                    >
+                      <path d='M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34zm192-34l-136-136c-9.4-9.4-24.6-9.4-33.9 0l-22.6 22.6c-9.4 9.4-9.4 24.6 0 33.9l96.4 96.4-96.4 96.4c-9.4 9.4-9.4 24.6 0 33.9l22.6 22.6c9.4 9.4 24.6 9.4 33.9 0l136-136c9.4-9.2 9.4-24.4 0-33.8z'></path>
+                    </svg>
+                    <p className='detail'>{para2}</p>
+                  </div>
+
+                  <div className='desc'>
+                    <svg
+                      stroke='currentColor'
+                      fill='currentColor'
+                      stroke-width='0'
+                      viewBox='0 0 448 512'
+                      className='icon'
+                      height='1em'
+                      width='1em'
+                      xmlns='http://www.w3.org/2000/svg'
+                    >
+                      <path d='M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34zm192-34l-136-136c-9.4-9.4-24.6-9.4-33.9 0l-22.6 22.6c-9.4 9.4-9.4 24.6 0 33.9l96.4 96.4-96.4 96.4c-9.4 9.4-9.4 24.6 0 33.9l22.6 22.6c9.4 9.4 24.6 9.4 33.9 0l136-136c9.4-9.2 9.4-24.4 0-33.8z'></path>
+                    </svg>
+                    <p className='detail'>{para3}</p>
+                  </div>
+
+                  {para4 && (
+                    <div className='desc'>
+                      <svg
+                        stroke='currentColor'
+                        fill='currentColor'
+                        stroke-width='0'
+                        viewBox='0 0 448 512'
+                        className='icon'
+                        height='1em'
+                        width='1em'
+                        xmlns='http://www.w3.org/2000/svg'
+                      >
+                        <path d='M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34zm192-34l-136-136c-9.4-9.4-24.6-9.4-33.9 0l-22.6 22.6c-9.4 9.4-9.4 24.6 0 33.9l96.4 96.4-96.4 96.4c-9.4 9.4-9.4 24.6 0 33.9l22.6 22.6c9.4 9.4 24.6 9.4 33.9 0l136-136c9.4-9.2 9.4-24.4 0-33.8z'></path>
+                      </svg>
+                      <p className='detail'>{para4}</p>
+                    </div>
+                  )}
+                </div>
+              </article>
+            )
+          }
+          return null
+        })}
+      </section>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/data.js
@@ -0,0 +1,43 @@
+export const names = [
+  {
+    job: 'full stack web developer',
+    name: 'bassey',
+    date: 'december 2015 - present',
+    para1:
+      'Tote bag sartorial mlkshk air plant vinyl banjo lumbersexual poke leggings offal cold-pressed brunch neutra. Hammock photo booth live-edge disrupt.',
+    para2:
+      'Post-ironic selvage chambray sartorial freegan meditation. Chambray chartreuse kombucha meditation, man bun four dollar toast street art cloud bread live-edge heirloom.',
+    para3:
+      'Butcher drinking vinegar franzen authentic messenger bag copper mug food truck taxidermy. Mumblecore lomo echo park readymade iPhone migas single-origin coffee franzen cloud bread tilde vegan flexitarian.',
+    category: 'bassey',
+    index: 1,
+  },
+  {
+    job: 'front-end engineer',
+    name: 'olisa',
+    date: 'may 2015 - december 2015',
+    para1:
+      'Hashtag drinking vinegar scenester mumblecore snackwave four dollar toast, lumbersexual XOXO. Cardigan church-key pabst, biodiesel vexillologist viral squid.',
+    para2:
+      'Franzen af pitchfork, mumblecore try-hard kogi XOXO roof party la croix cardigan neutra retro tattooed copper mug. Meditation lomo biodiesel scenester',
+    para3:
+      'Fam VHS enamel pin try-hard echo park raw denim unicorn fanny pack vape authentic. Helvetica fixie church-key, small batch jianbing messenger bag scenester +1',
+    para4:
+      'Fam VHS enamel pin try-hard echo park raw denim unicorn fanny pack vape authentic. Helvetica fixie church-key, small batch jianbing messenger bag scenester +1',
+    category: 'olisa',
+    index: 2,
+  },
+  {
+    job: 'engineering intern',
+    name: 'miracle',
+    date: 'may 2014 - september 2015',
+    para1:
+      "I'm baby woke mumblecore stumptown enamel pin. Snackwave prism pork belly, blog vape four loko sriracha messenger bag jean shorts DIY bushwick VHS. Banjo post-ironic hella af, palo santo craft beer gluten-free.",
+    para2:
+      'YOLO drinking vinegar chambray pok pok selfies quinoa kinfolk pitchfork street art la croix unicorn DIY. Woke offal jianbing venmo tote bag, palo santo subway tile slow-carb post-ironic pug ugh taxidermy squid.',
+    para3:
+      'Pour-over glossier chambray umami 3 wolf moon. Iceland kale chips asymmetrical craft beer actually forage, biodiesel tattooed fingerstache. Pork belly lomo man braid, portland pitchfork locavore man bun prism.',
+    category: 'miracle',
+    index: 3,
+  },
+]
